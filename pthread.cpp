c8935b5aac1237e163cf8ef018180dd3c8a76a4e
rupesh <iiita.rupeshyadav@gmail.com>
2014-08-03 12:48:02 +0530

    first commit

---
+++ pthread.cpp
@@ -0,0 +1,31 @@
+
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#define NTHREADS 4
+#define N 1000
+#define MEGEXTRA 1000000
+ 
+using namespace std;
+pthread_attr_t attr;
+ 
+void *dowork(void *threadid)
+{
+   for(;;){
+      cout << "thread running";
+   }
+}
+ 
+int main(int argc, char *argv[])
+{
+   pthread_t thread1;
+   pthread_attr_t thr_attr;
+   pthread_attr_init(&thr_attr);
+
+   pthread_create(&thread1,&thr_attr,dowork,(void *)NULL);
+   pthread_join(thread1,NULL);
+   pthread_exit(NULL);
+}
